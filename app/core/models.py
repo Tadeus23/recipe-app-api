4bd49740c2e244ea9f7f33d6a18e559cd369d6d3
Tadeus23 <popescu351@gmail.com>
2024-04-05 15:27:36 +0300

    Django admin setup 	modified:   app/core/admin.py 	modified:   app/core/models.py 	modified:   app/core/tests/test_admin.py 	modified:   app/core/tests/test_models.py

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     """Manager for users."""
-    
+
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new user."""
         if not email:
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
 
     def create_superuser(self, email, password):
@@ -28,18 +28,18 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-        
+
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
-    
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
